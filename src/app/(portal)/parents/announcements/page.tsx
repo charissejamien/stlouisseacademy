e8070789f60793d70b9dafe9766d4417a0c164b0
Charisse <jamiencharisse@gmail.com>
2026-05-09 11:49:25 +0800

    feat(parents): add dashboard and student profile page

---
+++ src/app/(portal)/parents/announcements/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Announcements() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
--- src/app/(portal)/parents/dashboard/page.tsx
+++ src/app/(portal)/parents/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import StudentSummary from "@/components/parents/StudentSummary";
+import StudentSummary from "@/components/parents/StudentSummary"
+import AnnouncementsWidget from "@/components/parents/AnnouncementsWidget";
 import Link from "next/link";
 
 
@@ -6,6 +7,10 @@ export default function Dashboard() {
     return (
         <div className="w-[80%] mx-auto mt-20 flex flex-col gap-5">
             <h2 className="text-[24px] font-medium">Welcome, Parent!</h2>
+
+            <div>
+                <AnnouncementsWidget/>
+            </div>
 
             <div>
                 <h3 className="text-[20px] text-sla-blue font-semibold">Student Profiles</h3>
